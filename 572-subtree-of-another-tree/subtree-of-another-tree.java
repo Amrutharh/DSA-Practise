fe203144e7b44eba33a03dcf47ccd8a3a4e5600e
Amrutha Rh <amrutharh28@gmail.com>
2026-09-03 21:18:56 +0530

    Time: 21 ms (5.71%) | Memory: 47 MB (6.63%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+    return serialize(s).contains(serialize(t)); // Java uses a naive contains algorithm so to ensure linear time, 
+                                                // replace with KMP algorithm
+}
+
+public String serialize(TreeNode root) {
+    StringBuilder res = new StringBuilder();
+    serialize(root, res);
+    return res.toString();
+}
+
+private void serialize(TreeNode cur, StringBuilder res) {
+    if (cur == null) {res.append(",#"); return;}
+    res.append("," + cur.val);
+    serialize(cur.left, res);
+    serialize(cur.right, res);
+}
+    
+}
